eb68afffceac4b3d4d360bbc039d2223bb541b9b
Huda <h.kukaswadia@lancaster.ac.uk>
2022-06-01 19:05:16 +0100

    Initial commit

---
+++ Trees/145.BinaryTreePostorderTraversal.cpp
@@ -0,0 +1,42 @@
+// Iterative PostOrder Traversal using 1 stack
+
+class Solution
+{
+public:
+    vector<int> postorderTraversal(TreeNode *root)
+    {
+
+        vector<int> postOrder;
+        if (root == NULL)
+            return postOrder;
+
+        stack<TreeNode *> st;
+        while (root != NULL || !st.empty())
+        {
+            if (root != NULL)
+            {
+                st.push(root);
+                root = root->left;
+            }
+            else
+            {
+                TreeNode *temp = st.top()->right;
+                if (temp == NULL)
+                {
+                    temp = st.top();
+                    st.pop();
+                    postOrder.push_back(temp->val);
+                    while (!st.empty() && temp == st.top()->right)
+                    {
+                        temp = st.top();
+                        st.pop();
+                        postOrder.push_back(temp->val);
+                    }
+                }
+                else
+                    root = temp;
+            }
+        }
+        return postOrder;
+    }
+};
