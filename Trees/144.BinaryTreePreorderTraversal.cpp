3172a120c4563174c5aa12afb46c224529ae9b0a
Huda <h.kukaswadia@lancaster.ac.uk>
2022-06-09 10:15:38 +0100

    Create 144. Binary Tree Preorder Traversal

---
+++ Trees/144.BinaryTreePreorderTraversal.cpp
@@ -0,0 +1,30 @@
+// Time complexity: O(n)
+// Space complexity: O(n)
+
+//Iterative
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> preOrder;
+        if (root == NULL) {
+            return preOrder;
+        }
+        
+        stack<TreeNode*> s;
+        s.push(root);
+        
+        while (!s.empty()) {
+            TreeNode* topNode = s.top();
+            preOrder.push_back(topNode -> val);
+            s.pop();
+            if (topNode -> right != NULL) {
+                s.push(topNode -> right);
+            }
+            
+            if(topNode -> left != NULL) {
+                s.push(topNode -> left);
+            }
+        }
+        return preOrder;
+    }            
+};
