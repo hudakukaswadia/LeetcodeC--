8002af2b08470d22f587a0d1f37ce581ccaf05b2
Huda <h.kukaswadia@lancaster.ac.uk>
2022-06-11 12:24:05 +0100

    update 94. Binary Tree Inorder Traversal

--- Trees/94.BinaryTreeInorderTraversal.cpp
+++ Trees/94.BinaryTreeInorderTraversal.cpp
@@ -1,7 +1,36 @@
 // Time complexity: O(n)
 // Space complexity: O(n)
 
+// Iterative
+
+class Solution {
+public:    
+    vector<int> inorderTraversal(TreeNode* root) {
+        stack<TreeNode*> st;
+        TreeNode* node = root;
+        vector<int> inorder;
+        while(true) {
+            if(node != NULL) {
+                st.push(node);
+                node = node -> left;
+            }
+            else {
+                if (st.empty() == true) {
+                    break;
+                }
+                node = st.top();
+                st.pop();
+                inorder.push_back(node -> val);
+                node = node -> right;
+            }
+        }
+        return inorder;
+    }
+};
+
+
 // Recursive
+
 class Solution {
 public:
     void inOrder(TreeNode* root, vector<int> &res){
@@ -19,3 +48,4 @@ public:
         return res;
     }
 };
+
