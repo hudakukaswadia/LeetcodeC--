07eb763062fed77f20b6cea80d8b33a5fb4c0ff4
Huda <h.kukaswadia@lancaster.ac.uk>
2022-06-12 21:51:59 +0100

    update leetcode 145

--- Trees/145.BinaryTreePostorderTraversal.cpp
+++ Trees/145.BinaryTreePostorderTraversal.cpp
@@ -22,6 +22,44 @@ public:
     }
 };
 
+// Iterative PostOrder Traversal using 2 Stacks
+
+// Time complexity: O(N)
+// Space complexity: O(2N)
+
+class Solution {
+public: 
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> postOrder;
+        if (root == NULL) {
+            return postOrder;
+        }
+        
+        stack<TreeNode*> st1;
+        stack<TreeNode*> st2;
+        
+        st1.push(root);
+        
+        while (!st1.empty()) {
+            root = st1.top();
+            st1.pop();
+            st2.push(root);
+            if (root -> left != NULL) {
+                st1.push(root -> left);
+            }
+            if (root -> right != NULL) {
+                st1.push(root -> right);
+            }
+        }
+        
+        while (!st2.empty()) {
+            postOrder.push_back(st2.top() -> val);
+            st2.pop();
+        }
+        return postOrder;
+    }     
+};
+
 
 // Iterative PostOrder Traversal using 1 stack
 
